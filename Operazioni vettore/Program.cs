cb8cdb3d3be74a80ec969f67315bab54234682a4
atadd <atadd@DESKTOP-PVF3S8C>
2024-02-20 17:45:20 +0100

    Aggiungere i file di progetto.

---
+++ Operazioni vettore/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operazioni_vettore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Operazioni vettore/Vector.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operazioni_vettore
+{
+    internal class Vector
+    {
+        public Vector(double x, double y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public double X { get; set; }
+        public double Y { get; set; }
+        public static Vector Parse(string s)
+        {
+            var a=s.Split(',');
+            var b= new Vector(int.Parse(a[0]), int.Parse(a[1]));
+            return b;
+        }
+        public static Vector operator +(Vector a, Vector b)
+        {
+            return new Vector((a.X+b.X), (a.Y+b.Y));
+        }
+        public static Vector operator -(Vector a, Vector b)
+        {
+            return new Vector((a.X - b.X), (a.Y - b.Y));
+        }
+        public static Vector operator *(Vector a,Vector b)
+        {
+            return new Vector((a.X*b.X),(a.Y*b.Y));
+        }
+        public static Vector operator -(Vector a) {
+            return new Vector((-a.X), (-a.Y));
+        }
+        public static Vector operator +(Vector a)
+        {
+            return new Vector((+a.X), (+a.Y));
+        }
+        public static Vector operator *(Vector a,double s)
+        {
+            return new Vector((a.X * s), (a.Y * s));
+        }
+
+    }
+}
